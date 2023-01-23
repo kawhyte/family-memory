c549d3929c94f1280df7d6ed3f2d48bbd322dc26
Kenny Whyte <kawhyte@gmail.com>
2023-01-23 15:37:20 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+	{ src: "/img/helmet-1.png" },
+	{ src: "/img/potion-1.png" },
+	{ src: "/img/ring-1.png" },
+	{ src: "/img/scroll-1.png" },
+	{ src: "/img/sheild-1.png" },
+	{ src: "/img/sword-1.png" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const[cards, setCards] = useState([])
+  const[turns, setTurns] = useState(0)
+
+	//shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+
+      setCards (shuffledCards)
+      setTurns(0)
+	};
+
+  console.log("Cards",cards)
+  console.log("Turns", turns)
+
+	return (
+		<div className='App'>
+			<h1>Matching Game</h1>
+			<button onClick={shuffleCards} >New Game</button>
+		</div>
+	);
 }
 
 export default App;
