d0417156d9f1777ecf2b8838394821d825fd1881
Kenny Whyte <kawhyte@gmail.com>
2023-01-24 10:01:06 -0800

    added code to check  if cards match

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
 	{ src: "/img/helmet-1.png" },
 	{ src: "/img/potion-1.png" },
 	{ src: "/img/ring-1.png" },
 	{ src: "/img/scroll-1.png" },
-	{ src: "/img/sheild-1.png" },
+	{ src: "/img/shield-1.png" },
 	{ src: "/img/sword-1.png" },
 ];
 
 function App() {
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
 
-  const[cards, setCards] = useState([])
-  const[turns, setTurns] = useState(0)
+	const [choiceOne, setChoiceOne] = useState(null);
+	const [choiceTwo, setChoiceTwo] = useState(null);
 
-	//shuffle cards
+	// shuffle cards
 	const shuffleCards = () => {
 		const shuffledCards = [...cardImages, ...cardImages]
 			.sort(() => Math.random() - 0.5)
 			.map((card) => ({ ...card, id: Math.random() }));
 
-      setCards (shuffledCards)
-      setTurns(0)
+		setCards(shuffledCards);
+		setTurns(0);
 	};
 
-  console.log("Cards",cards)
-  console.log("Turns", turns)
+	// handle choice
+	const handleChoice = (card) => {
+		console.log("handleChoice", card);
 
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+	};
+
+	// compare cards
+useEffect(()=> {
+if (choiceOne && choiceTwo) {
+  if(choiceOne.src === choiceTwo.src){
+    console.log("the cards match")
+    resetTurn()
+  } else{
+    console.log("the cards DONT match")
+    resetTurn()
+  }
+}
+
+},[choiceOne, choiceTwo])
+
+
+	// rest choices and increase turn
+	const resetTurn = () => {
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setTurns((prevTurns) => prevTurns + 1);
+	};
 	return (
 		<div className='App'>
 			<h1>Matching Game</h1>
-			<button onClick={shuffleCards} >New Game</button>
+			<button onClick={shuffleCards}>New Game</button>
+			<div className='card-grid'>
+				{cards.map((card) => (
+					<SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+				))}
+			</div>
 		</div>
 	);
 }
