6b4ce757d1b63180dc3401e8648f163273f82fc8
Kenny Whyte <kawhyte@gmail.com>
2023-01-24 10:39:35 -0800

    added simple card animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-	{ src: "/img/helmet-1.png" },
-	{ src: "/img/potion-1.png" },
-	{ src: "/img/ring-1.png" },
-	{ src: "/img/scroll-1.png" },
-	{ src: "/img/shield-1.png" },
-	{ src: "/img/sword-1.png" },
+	{ src: "/img/helmet-1.png", matched: false },
+	{ src: "/img/potion-1.png", matched: false },
+	{ src: "/img/ring-1.png", matched: false },
+	{ src: "/img/scroll-1.png", matched: false },
+	{ src: "/img/shield-1.png", matched: false },
+	{ src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -17,7 +17,8 @@ function App() {
 
 	const [choiceOne, setChoiceOne] = useState(null);
 	const [choiceTwo, setChoiceTwo] = useState(null);
-
+	const [disabled, setDisabled] = useState(false);
+ 
 	// shuffle cards
 	const shuffleCards = () => {
 		const shuffledCards = [...cardImages, ...cardImages]
@@ -36,25 +37,35 @@ function App() {
 	};
 
 	// compare cards
-useEffect(()=> {
-if (choiceOne && choiceTwo) {
-  if(choiceOne.src === choiceTwo.src){
-    console.log("the cards match")
-    resetTurn()
-  } else{
-    console.log("the cards DONT match")
-    resetTurn()
-  }
-}
+	useEffect(() => {
+    
+		if (choiceOne && choiceTwo) {
+      setDisabled(true)
+			if (choiceOne.src === choiceTwo.src) {
+				setCards((prevCards) => {
+					return prevCards.map((card) => {
+						if (card.src === choiceOne.src) {
+							return { ...card, matched: true };
+						} else {
+							return card;
+						}
+					});
+				});
+				resetTurn();
+			} else {
+				setTimeout(() => resetTurn(), 2000);
+			}
+		}
+	}, [choiceOne, choiceTwo]);
 
-},[choiceOne, choiceTwo])
-
+	console.log("Cards State", cards);
 
 	// rest choices and increase turn
 	const resetTurn = () => {
 		setChoiceOne(null);
 		setChoiceTwo(null);
 		setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false)
 	};
 	return (
 		<div className='App'>
@@ -62,7 +73,13 @@ if (choiceOne && choiceTwo) {
 			<button onClick={shuffleCards}>New Game</button>
 			<div className='card-grid'>
 				{cards.map((card) => (
-					<SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+					<SingleCard
+						key={card.id}
+						card={card}
+						handleChoice={handleChoice}
+						flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+					/>
 				))}
 			</div>
 		</div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./SingleCard.css";
 
-export default function SingleCard({ card, handleChoice }) {
-
-    const handleClick = () => {
-handleChoice(card)
-
-    }
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+	const handleClick = () => {
+        if(!disabled){
+            handleChoice(card);
+        }
+		
+	};
 
 	return (
 		<div className='card'>
-			<div>
+			<div className={flipped ? "flipped" : ""}>
 				<img className='front' src={card.src} alt='card front' />
-				
-				<img className='back' src='/img/cover.png' onClick={handleClick} alt='card back' />
+
+				<img
+					className='back'
+					src='/img/cover.png'
+					onClick={handleClick}
+					alt='card back'
+				/>
 			</div>
 		</div>
 	);
